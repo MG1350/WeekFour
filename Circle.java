fda1c452515ee4003dbf75d7916334059192031f
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-26 14:45:46 -0500

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle 
+{
+    public static void main(String[] args)
+    {
+        double radius = 4.28;
+        double area = 3.14 * radius * radius;
+        double circum = 2 * 3.14 * radius;
+        System.out.println("The area of the circle is "+ area);
+        System.out.println("The circumfrence of the circle is "+ circum);
+    }
+}
