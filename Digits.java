16f3d51cfcc39fadd8f35c253683d2a62897a848
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-27 15:00:33 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -6,8 +6,20 @@ public class Digits
         int ones = num % 10;
         int tens = num % 100 - ones;
         int hundreds = num % 1000 - tens - ones;
-        System.out.println("The hundreds-place digit is: " + hundreds);
-        System.out.println("The tens-place digit is: " + tens);
+        int hund = 0;
+        int tend = 0;
+        while (hundreds != 0)
+        {
+            hundreds = hundreds - 100;
+            hund = hund + +1;
+        }
+        while (tens != 0)
+        {
+            tens = tens - 10;
+            tend = tend + +1;
+        }
+        System.out.println("The hundreds-place digit is: " + hund);
+        System.out.println("The tens-place digit is: " + tend);
         System.out.println("The ones-place digit is: " + ones);
 
     }
